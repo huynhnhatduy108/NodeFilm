613a16e8bfe58010a35377739fbc6dfc47ba7369
Nhat Duy <huynhnhatduy2112@gmail.com>
2021-01-09 18:35:59 +0700

    update typefilm

--- models/type.js
+++ models/type.js
@@ -3,13 +3,17 @@ const mongoose = require('mongoose');
 const TypeSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, 'Name film is required']
+        required: true
     },
     description: {
         type: String,
+        required: true
+
     },
     keycode: {
         type: String,
+        required: true
+
     },
     films: [{
         type: mongoose.Schema.Types.ObjectId,
